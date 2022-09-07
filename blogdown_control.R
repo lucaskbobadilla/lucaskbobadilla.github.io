e19d2bcbf919bd153d8ccc38ec805437d0a405e3
lucaskbobadilla <lucaskbobadilla@gmail.com>
2022-09-06 20:38:15 -0500

    update blogdown

--- blogdown_control.R
+++ blogdown_control.R
@@ -3,5 +3,9 @@ blogdown::serve_site()
 blogdown::stop_server()
 blogdown::check_site()
 
-blogdown::config_Rprofile()
-options(blogdown.hugo.version = "0.97.3")
+
+
+blogdown::edit_draft(c(
+  "content/privacy.md",
+  "content/terms.md"
+))
